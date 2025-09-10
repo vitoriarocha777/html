f95aeb0d6e626b59c87772d0c02a7028335dfb4a
vitoriarocha777 <vitoria.rocha29@escola.pr.gov.br>
2025-09-10 09:40:03 -0300

    Add files via upload

--- TAVARES/script.js
+++ TAVARES/script.js
@@ -1,19 +1,25 @@
-function calculadoratabuada() {
+function calculadoraTabuada() {
     let numero = document.getElementById("numero").value;
     let resultadoDiv=document.getElementById("resultado");
-}
+
 
 if (numero==="") {
     resultadoDiv.innerHTML = "<p style='color:red'>Digite um numero valido!</p>";
 } else{
     let resultadoHTML = "<h3>Tabuada do" + numero + ":<h3>";
     let i = 1;
-}
+
 
 while (i <= 10) {
-    resultadoHTML += "<p>" + numero + " x " + i + "=" + (numero * i) + ",/p>";
+    resultadoHTML += "<p>" + numero + " x " + i + "=" + (numero * i) + "</p>";
     i++;
     
 }
 
 resultadoDiv.innerHTML = resultadoHTML;
+  }
+}
+
+ 
+
+ 
