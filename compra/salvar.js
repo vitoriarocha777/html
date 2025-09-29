790ff832b23fae9cdfc0d68010b9e672f2912733
vitoriarocha777 <vitoria.rocha29@escola.pr.gov.br>
2025-09-29 12:09:21 -0300

    Add files via upload

--- compra/salvar.js
+++ compra/salvar.js
@@ -5,7 +5,7 @@ btnSalvar.style.display = 'block';
 btnSalvar.style.margin = '20px auto';
 btnSalvar.style.padding = '6px 12px';
 btnSalvar.style.cursor = 'pointer';
-container.appendChild(btnSalvar);
+conteiner.appendChild(btnSalvar);
 
 // Função para salvar a lista em um arquivo .txt
 function salvarLista () {
@@ -13,26 +13,26 @@ function salvarLista () {
     const itens = Array.from(lista.querySelectorAll('li'))
                             . map(li => li.textContent)
                             .join('\n');
- if (!item) {
-    alert('A lista esta vazia!');
-    return;
- }
+   if (!item) {
+      alert('A lista esta vazia!');
+     return;
+    }
 
 
- //Cria uma Blob com o conteudo de texto
- const blob = new Blob((itens),{ typer: 'text/plain'});
- const url = URL.createObjectURL(blob);
+      //Cria uma Blob com o conteudo de texto
+      const blob = new Blob((itens),{ typer: 'text/plain'});
+      const url = URL.createObjectURL(blob);
 
- // Cria uma link temporario para downloand
- const a = document.createElement('a');
- a.href = url;
- a.download = 'lista.txt'; //none do arquivo
- document.body.appendChild(a);
- a.click(); //dispara o download
- document.body.removeChild(a);
+    // Cria uma link temporario para downloand
+       const a = document.createElement('a');
+       a.href = url;
+      a.download = 'lista.txt'; //none do arquivo
+     document.body.appendChild(a);
+      a.click(); //dispara o download
+       document.body.removeChild(a);
 
- //Libera o objeto URL
- URL.revokeObjectURL(url);
+      //Libera o objeto URL
+    URL.revokeObjectURL(url);
 }
 
 //Evento de botão
